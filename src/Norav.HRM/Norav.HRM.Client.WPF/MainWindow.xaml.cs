56ec3c37d4e31ccfc5b3e65ae4856aea6d9334ba
Shlomi Ohayon <cl_shlomio@rnd-hub.com>
2021-12-23 14:54:40 +0200

    + ExampleData\ECG.data.csv plot POC

--- src/Norav.HRM/Norav.HRM.Client.WPF/MainWindow.xaml.cs
+++ src/Norav.HRM/Norav.HRM.Client.WPF/MainWindow.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace Norav.HRM.Client.WPF
+﻿using System.IO;
+using System.Linq;
+
+namespace Norav.HRM.Client.WPF
 {
     public partial class MainWindow 
     {
         public MainWindow()
         {
             InitializeComponent();
+
+#if false
+            string[] readAllLines = File.ReadAllLines(@"ExampleData\ECG.data.csv");
+            double[] dataY = readAllLines.Take(10*1024).Select(double.Parse).ToArray();
+            double[] dataX = Enumerable.Range(0, dataY.Length).Select(i => (double)i).ToArray();
+            WpfPlot1.Plot.AddScatter(dataX, dataY);
+#endif
+
+            WpfPlot1.Refresh();
         }
     }
 }
